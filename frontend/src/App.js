6922e120c9eb7676f81004931ccdc66d55566867
kennedyuc <chukwukakennedyuche@gmail.com>
2022-12-22 05:12:44 -0800

    frontend scripts updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,46 @@
-// @bekbrace
-// FARMSTACK Tutorial - Sunday 13.06.2021
- 
-import React, { useState, useEffect} from 'react';
-import './App.css';
-import TodoView from './components/TodoListView';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { Themes, GLobalStyles } from "./theme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Stores
+import { Store } from "./store/contexts/AppContext";
+
+// pages
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Example from "./pages/Example";
+import Docs from "./pages/Docs";
+import Aboutus from "pages/Aboutus";
+import FAQ from "./pages/FAQ";
+import CsvUpload from "pages/CsvUpload";
+import HelpCenter from "pages/helpCenter/HelpCenter";
+import SignUp from "./pages/auth/SignUp";
+import Privacy from "./pages/Privacy";
+import Mission from "pages/Mission";
 
 function App() {
-
-  const [todoList, setTodoList] = useState([{}])
-  const [title, setTitle] = useState('') 
-  const [desc, setDesc] = useState('')
-  
-    
-
-  // Read all todos
-  useEffect(() => {
-    axios.get('/api/todo')
-      .then(res => {
-        setTodoList(res.data)
-      })
-  });
-
-  // Post a todo
-  const addTodoHandler = () => {
-    axios.post('/api/todo/', { 'title': title, 'description': desc })
-      .then(res => console.log(res))
-};
+  const { theme } = Store();
 
   return (
-    <div className="App list-group-item  justify-content-center align-items-center mx-auto" style={{"width":"400px", "backgroundColor":"white", "marginTop":"15px"}} >
-      <h1 className="card text-white bg-primary mb-1" styleName="max-width: 20rem;">Task Manager</h1>
-      <h6 className="card text-white bg-primary mb-3">FASTAPI - React - MongoDB</h6>
-     <div className="card-body">
-      <h5 className="card text-white bg-dark mb-3">Add Your Task</h5>
-      <span className="card-text"> 
-        <input className="mb-2 form-control titleIn" onChange={event => setTitle(event.target.value)} placeholder='Title'/> 
-        <input className="mb-2 form-control desIn" onChange={event => setDesc(event.target.value)}   placeholder='Description'/>
-      <button className="btn btn-outline-primary mx-2 mb-3" style={{'borderRadius':'50px',"font-weight":"bold"}}  onClick={addTodoHandler}>Add Task</button>
-      </span>
-      <h5 className="card text-white bg-dark mb-3">Your Tasks</h5>
-      <div >
-      <TodoView todoList={todoList} />
-      </div>
-      </div>
-      <h6 className="card text-dark bg-warning py-1 mb-0" >Copyright 2021, All rights reserved &copy;</h6>
-    </div>
+    <ThemeProvider theme={theme ? Themes.light : Themes.dark}>
+      <GLobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/example" element={<Example />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/docs/*" element={<Docs />} />
+          <Route path="/about" element={<Aboutus />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/mission" element={<Mission />} />
+          <Route path="/csv-upload" element={<CsvUpload />} />
+          <Route path="/help" element={<HelpCenter />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
